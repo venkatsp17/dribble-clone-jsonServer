112c25b3a8bd47ac575aaa1bba24bddb09bf7294
venkatsp17 <venkatsp2002@gmail.com>
2024-10-04 00:51:20 +0530

    added cors config

--- server.cjs
+++ server.cjs
@@ -2,10 +2,11 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 
 app.use(bodyParser.json());
 
